2312a3b4eaa460c7ed3e5035344993d4418a49bb
Asher <asher.bernardi@my.wheaton.edu>
2019-03-28 23:00:23 -0500

    feed forward done

--- mlp.py
+++ mlp.py
@@ -3,6 +3,8 @@ from random import uniform
 
 import perceptron
 
+Perceptron = perceptron.Perceptron
+
 # a class that represents one hidden layer in a neural network
 class PerceptronLayer:
     def __init__(self, perceptrons):
@@ -15,13 +17,24 @@ class PerceptronLayer:
 class MultiLayerPerceptron:
     def __init__(self, layers):
         self.layers = layers
-    def feed_forward(self, inputs):
-        for layer in self.layers:
-            for perceptron in layer.perceptrons:
-                
     def initialize(layer_sizes, num_weights, activation):
         return MultiLayerPerceptron([PerceptronLayer.initialize(num_perceptrons,
                                      num_weights, activation) for num_perceptrons in layer_sizes])
+    def feed_forward(self, inputs):
+        outputs = []
+        for layer in self.layers:
+            output = [perceptron(inputs)
+                      for perceptron in layer.perceptrons]
+            outputs.append(output)
+            inputs = output
+        return outputs
+    def single_layer_backprop(self, inputs, targets, eta=0.1):
+        outputs = self.feed_forward(inputs)
+        delta_y = [yl * (1 - yl) * (ti - yl) for yl,ti in zip(outputs[1], targets)]
+        delta_z = [zk * (1 - zk) *
+                    np.dot(delta_y, [self.layers[1].perceptrons[l].weights[k]
+                    for l in range(len(self.layers[1].perceptrons))])
+                    for k,zk in enumerate(outputs[0])]
 
 # p is an UnthreasholdedPerceptron
 def stochastic_gradient_descent(data, targets, termination = 100,
@@ -34,7 +47,14 @@ def stochastic_gradient_descent(data, targets, termination = 100,
 # Backpropagation with only one hidden layer
 def train(M, data, targets, termination = 100):
     for k in range(M):
-        for
+        pass
 
 # feed forward
 def classify(mlp, inputs):
+    return mlp.feed_forward(inputs)[-1]
+
+# xor_network = MultiLayerPerceptron([PerceptronLayer([Perceptron([-30,20,20],perceptron.sigmoid),Perceptron([-10,20,20],perceptron.sigmoid)]),PerceptronLayer([Perceptron([-30,-60,60],perceptron.sigmoid)])])
+
+# for x in [0,1]:
+#     for y in [0,1]:
+#         print x, y, xor_network.feed_forward([x,y])[-1]
--- perceptron.py
+++ perceptron.py
@@ -24,7 +24,7 @@ class Perceptron :
     def initialize(num_weights, activation):
         return Perceptron([uniform(-1,1) for i in range(num_weights)], activation)
     def __call__(self, inputs) :
-        return [self.activation(j) for j in np.dot(self.weights, [1]+inputs)]
+        return self.activation(np.dot(self.weights, [1]+inputs))
     def __str__(self) :
         return ",".join([str(w) for w in self.weights])
 
@@ -56,45 +56,45 @@ def perc_train_step(p, x, t, eta=.1) :
 
 
 
-line = np.linspace(-5, 5, 100)
-import matplotlib.pyplot as plt
+# line = np.linspace(-5, 5, 100)
+# import matplotlib.pyplot as plt
 
-# The other activation functions are already in numpy
+# # The other activation functions are already in numpy
 
-# Let's see the 0/1 activation functions
+# # Let's see the 0/1 activation functions
 
-plt.plot(line, [step(x) for x in line], label="step")
-plt.plot(line, [sigmoid(x) for x in line], label="sigmoid")
-plt.legend(loc="best")
-plt.show()
+# plt.plot(line, [step(x) for x in line], label="step")
+# plt.plot(line, [sigmoid(x) for x in line], label="sigmoid")
+# plt.legend(loc="best")
+# plt.show()
 
-# Now let's see the -1/1 activation functions
+# # Now let's see the -1/1 activation functions
 
-plt.plot(line, np.tanh(line), label="tanh")
-plt.plot(line, np.sign(line), label="sign")
-plt.plot(line, [sigmoid_adjusted(x) for x in line], label="sigmoid")
-plt.legend(loc="best")
-plt.show()
-
-
-and_gate = Perceptron([-1, 1, 1], np.tanh)
-
-[and_gate([1,1]),and_gate([1,-1]),and_gate([-1,-1])]
-
-or_gate = Perceptron([1, 1, 1], np.tanh)
-
-[or_gate([1,1]), or_gate([1,-1]), or_gate([-1,-1])]
+# plt.plot(line, np.tanh(line), label="tanh")
+# plt.plot(line, np.sign(line), label="sign")
+# plt.plot(line, [sigmoid_adjusted(x) for x in line], label="sigmoid")
+# plt.legend(loc="best")
+# plt.show()
 
 
+# and_gate = Perceptron([-1, 1, 1], np.tanh)
+
+# [and_gate([1,1]),and_gate([1,-1]),and_gate([-1,-1])]
+
+# or_gate = Perceptron([1, 1, 1], np.tanh)
+
+# [or_gate([1,1]), or_gate([1,-1]), or_gate([-1,-1])]
 
 
 
-# train an and gate
-data = [([1,1],1), ([1,-1],-1), ([-1,1],-1), ([-1,-1],-1)]
-p = initialize_perceptron(3)
-for i in range(10) :
-    print("iteration " + str(i))
-    print(str(p))
-    print(",".join([str(p(d[0])) for d in data]))
-    for d in data:
-        perc_train_step(p, d[0], d[1])
+
+
+# # train an and gate
+# data = [([1,1],1), ([1,-1],-1), ([-1,1],-1), ([-1,-1],-1)]
+# p = initialize_perceptron(3)
+# for i in range(10) :
+#     print("iteration " + str(i))
+#     print(str(p))
+#     print(",".join([str(p(d[0])) for d in data]))
+#     for d in data:
+#         perc_train_step(p, d[0], d[1])
